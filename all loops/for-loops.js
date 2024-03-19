7550a75a7030350964e488c9684b8f16e5d5dfb5
Mustafa <mssheliyait@gmail.com>
2024-03-19 17:50:21 +0530

    leran About settimeout & setinterval & create project 5&6

--- all loops/for-loops.js
+++ all loops/for-loops.js
@@ -1,7 +1,7 @@
 let arr = ["mustfa","ahmed","sheliya","Mohammed",25];
 for (let i = 0; i < arr.length; i++) {
     // console.log(arr[i])
-    
+
 }
 
 // reverse the array without using .reverse() method
@@ -10,3 +10,17 @@ for (let i = arr.length-1; i >= 0; i--) {
     newarr.push(arr[i]);
 }
 // console.log(newarr);
+
+// reverse the String without using .reverse() method and without changing word index;
+let str = "my name Mustafa";
+let strtoArr = str.split(" ");
+let finalRes = [];
+for (let i = 0; i < strtoArr.length; i++) {
+    const element = strtoArr[i].split("");
+        let firstRes = "";
+        for (let j = element.length -1; j >= 0; j--) {
+            firstRes += element[j];
+        }
+   finalRes.push(firstRes);
+}
+// console.log(finalRes.join().replaceAll(",", " "));
